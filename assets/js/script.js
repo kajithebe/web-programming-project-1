5745985c8703e76b985ded16f7f275789807f90d
Kaji Thebe <kajithebe2000@gmail.com>
2025-10-02 18:36:11 +0300

    Remaining pages added and header and footer injected to all pages using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+function includeHTML() {
+  const includes = document.querySelectorAll('[id$="-placeholder"]');
+
+  includes.forEach((el) => {
+    const file = 'partials/' + el.id.replace('-placeholder', '.html');
+
+    fetch(file)
+      .then((response) => {
+        if (!response.ok) throw new Error('Network response was not ok');
+        return response.text();
+      })
+      .then((data) => {
+        el.innerHTML = data;
+      })
+      .catch((err) => console.log(err));
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   document.body.style.display = 'block';
+  includeHTML();
 });
